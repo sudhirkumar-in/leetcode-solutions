71ac94f4ed5616d7a6ebed125827f9e90ac88b4f
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-06-25 21:37:16 +0530

    Time: 385 ms (68.67%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -3,7 +3,7 @@
 with empman as (
     select x.id, x.name , x.salary , x.managerId,
     e.id as m_id,e.name as m_name, e.salary as m_salary
-    from Employee x CROSS JOIN Employee e 
+    from Employee x  JOIN Employee e ON x.managerId = e.id
 ),
 emp_man_only as(
     select * from empman e where e.managerid = e.m_id and e.salary > e.m_salary
