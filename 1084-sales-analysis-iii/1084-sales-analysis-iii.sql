25bb2ef53c5ed0e196b66add26b62e080a8ae5e6
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-06-26 18:19:09 +0530

    Time: 1119 ms (82.78%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -7,6 +7,6 @@ next as (
     select * from Sales where sale_date not between '2019-01-01' and '2019-03-31'
 ),
 firstQ as (
-    select * from y19 where product_id not in ( select product_id from next )
+    select distinct product_id from y19 where product_id not in ( select product_id from next )
 )
 select distinct p.product_id , p.product_name from Product p JOIN  firstQ f where p.product_id = f.product_id
