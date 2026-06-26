dfa8c9e7948c8eae3f99a58a9c4979b7fce451b1
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-06-26 18:17:52 +0530

    Time: 1243 ms (38.5%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+with 
+y19 as (
+    select * from Sales where sale_date between '2019-01-01' and '2019-03-31'
+),
+next as (
+    select * from Sales where sale_date not between '2019-01-01' and '2019-03-31'
+),
+firstQ as (
+    select * from y19 where product_id not in ( select product_id from next )
+)
+select distinct p.product_id , p.product_name from Product p JOIN  firstQ f where p.product_id = f.product_id
