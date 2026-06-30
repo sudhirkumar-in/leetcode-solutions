75a4b145679cd3b14aedf49b1d079589b09c1214
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-06-30 17:46:05 +0530

    Time: 1 ms (59.41%), Space: 55.8 MB (9.21%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -7,7 +7,11 @@ var sumOfUnique = function (nums) {
     let map = new Map();
     let sum = 0;
     for (const num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
+        let inMap = map.get(num) || 0;
+        if(inMap==2){
+            continue;
+        }
+        map.set(num, inMap + 1);
     }
 
     for (const [key, value] of map.entries()) {
