b8128c758c279e5adbd1995c5ce804aed3b44cfd
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-07 15:12:54 +0530

    Time: 0 ms (100%), Space: 57.9 MB (46.67%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.ts
@@ -0,0 +1,17 @@
+function sumAndMultiply(n: number): number {
+    if (n == 0) {
+        return 0;
+    }
+    let xStr = '';
+    let sum = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        n = Math.trunc(n / 10);
+        if (digit != 0) {
+            sum += digit;
+            xStr = digit + xStr;
+        }
+    }
+
+    return sum * parseInt(xStr);
+};
