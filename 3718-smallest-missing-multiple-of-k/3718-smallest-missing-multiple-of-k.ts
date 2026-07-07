4d4d7e6034c177752902b32ea7ef4b4061373beb
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-07 15:53:19 +0530

    Time: 1 ms (60%), Space: 57.6 MB (40%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.ts
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.ts
@@ -1,17 +1,15 @@
 function missingMultiple(nums: number[], k: number): number {
-    const mulOfK = [];
+    const set = new Set()
     for (const num of nums) {
         if (num % k == 0) {
             const ind = num / k
-            mulOfK[ind] = ind;
+            set.add(ind);
         }
     }
 
-    const len = mulOfK.length
-    for (let i = 1; i < len; i++) {
-        if (mulOfK[i] == undefined) {
-            return k * i;
-        }
+    let i = 1;
+    while (set.has(i)) {
+        i += 1;
     }
-    return len ? k * len : k;
+    return k * i;
 };
