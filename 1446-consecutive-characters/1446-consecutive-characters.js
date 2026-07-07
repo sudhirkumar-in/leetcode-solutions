6adfdaf064a412bb8f4c8c347d97564cbfb8aadd
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-07 19:45:17 +0530

    Time: 0 ms (100%), Space: 53.2 MB (95%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxPower = function (s) {
-    let chars = s.split('');
+    // let chars = s.split('');
     let best = 0;
     let count = 1;
-    for (let i = 0; i < chars.length - 1; i++) {
-        if (chars[i] == chars[i + 1]) {
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] == s[i + 1]) {
             count += 1;
         } else {
             best = Math.max(best, count);
