76c58c727282ee6d83f2a85775b3e4cac7ae6da2
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-16 15:41:15 +0530

    Time: 0 ms (100%), Space: 53.6 MB (61.04%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    let result = [];
+    let carry = 1; // its because we have to plus one
+    let once = true;
+    for (let last = digits.length - 1; last >= 0; last--) {
+        const sum = digits[last] + carry;
+        if (sum >= 10) {
+            let carry = 1;
+            const digit = sum % 10;
+            result.unshift(digit);
+        } else {
+            carry = 0;
+            result.unshift(sum);
+        }
+
+    }
+    if (carry) {
+        result.unshift(carry);
+    }
+    return result;
+};
