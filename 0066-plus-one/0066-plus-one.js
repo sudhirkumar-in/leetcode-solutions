f30bd0b6975d03990054885dfb7809f6e5449c91
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-16 15:44:04 +0530

    Time: 0 ms (100%), Space: 53.3 MB (72.98%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -5,11 +5,10 @@
 var plusOne = function (digits) {
     let result = [];
     let carry = 1; // its because we have to plus one
-    let once = true;
     for (let last = digits.length - 1; last >= 0; last--) {
         const sum = digits[last] + carry;
         if (sum >= 10) {
-            let carry = 1;
+            carry = 1;
             const digit = sum % 10;
             result.unshift(digit);
         } else {
