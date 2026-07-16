e5d4867230a6a79530f186a287164ad8c5ddf46f
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-16 16:24:21 +0530

    Time: 0 ms (100%), Space: 54.6 MB (73.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,8 +4,8 @@
  * @return {string}
  */
 var addBinary = function (a, b) {
-    const firstNum = BigInt("0b" + a, 2);
-    const secondNum = BigInt("0b" + b, 2);
+    const firstNum = BigInt("0b" + a);
+    const secondNum = BigInt("0b" + b);
     const sum = firstNum + secondNum;
     const sumBin = sum.toString(2);
     return sumBin;
