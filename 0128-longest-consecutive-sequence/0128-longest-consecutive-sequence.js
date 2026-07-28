733ef3018f63ac0feeb048907c92464a00179e41
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-07-28 14:10:13 +0530

    Time: 55 ms (11.96%), Space: 80.3 MB (72.2%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    nums = [...new Set(nums)]
+    nums.sort((a, b) => a - b);
+    nums.push(Infinity); // sentinal
+    let best = 0;
+    let count = 1;
+    for (let i = 0; i < nums.length - 1; i++) {
+        const curr = nums[i];
+        const next = nums[i + 1];
+
+        if (curr + 1 === next) {
+            count += 1;
+            continue;
+        }
+
+        best = Math.max(best, count);
+        count = 1; // reset
+    }
+    return best;
+};
