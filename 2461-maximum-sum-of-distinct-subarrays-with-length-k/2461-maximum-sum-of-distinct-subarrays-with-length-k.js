604d695dc45d915408e78dca94504dbfae467cb2
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-05 13:12:56 +0530

    Time: 66 ms (64.19%), Space: 89.9 MB (19.53%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.js
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.js
@@ -4,33 +4,32 @@
  * @return {number}
  */
 var maximumSubarraySum = function (nums, k) {
-    const windowMap = new Map();
 
-    let windowSum = 0;
-    let bestSum = 0;
-    let len = nums.length;
-    for (let i = 0; i < len; i++) {
-        // update window map , sub last  , then add new
-        let curr = nums[i]
-        windowMap.set(curr, (windowMap.get(curr) ?? 0) + 1)
-        windowSum += curr;
-
-        if (i >= k) {
-            let last = nums[i - k];
-            windowSum -= last
-            const count = windowMap.get(last);
-
-            if (count === 1) {
-                windowMap.delete(last);
-            } else {
-                windowMap.set(last, count - 1);
-            }
-        }
-        if (windowMap.size == k) {
-            bestSum = Math.max(bestSum, windowSum);
-        }
-
+    let freq = new Map();
+    let sum = 0;
+    for (let i = 0; (i < k && nums.length >= k); i++) {
+        sum += nums[i];
+        freq.set(nums[i], (freq.get(nums[i]) ?? 0) + 1)
     }
+    let maxSum = 0
+    if (freq.size === k) {
+        maxSum = sum;
+    }
+    const n = nums.length;
+    for (let i = k; i < n; i++) {
+        sum -= nums[i - k]
+        sum += nums[i]
+        if (freq.get(nums[i - k]) > 1) {
+            freq.set(nums[i - k], freq.get(nums[i - k]) - 1)
+        } else {
+            freq.delete(nums[i - k])
+        }
 
-    return bestSum
+        freq.set(nums[i], (freq.get(nums[i]) ?? 0) + 1);
+
+        if (freq.size === k) {
+            maxSum = Math.max(maxSum, sum);
+        }
+    }
+    return maxSum;
 };
