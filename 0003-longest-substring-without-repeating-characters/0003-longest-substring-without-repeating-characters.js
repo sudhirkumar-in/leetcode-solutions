2267cfd23b24b7a2ce5a08e0dca8b8d05932f804
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-05 15:43:53 +0530

    Time: 102 ms (14.73%), Space: 63.7 MB (5.76%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let left = 0;
+    let right = 0;
+    let best = 0;
+    const windowSet = new Set();
+    for (; right < s.length; right++) {
+        // get the curr and remove it and make window  valid
+        const curr = s[right];
+        while (windowSet.has(curr)) {
+            let el = s[left];
+            // remove it 
+            windowSet.delete(el);
+            // increaset the left 
+            left += 1;
+        }
+        // add the curr element to window
+        windowSet.add(curr);
+        // get the best 
+        best = Math.max(best, windowSet.size)
+
+    }
+
+
+    return best;
+};
