f20abde4bdbe5ae7661bd7a990ae5ac21c21f54b
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-06 06:11:01 +0530

    Time: 18 ms (62.19%), Space: 65.5 MB (23.69%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+    let count = 0;
+    let sum = 0;
+    const map = new Map();
+    map.set(0, 1);
+    for (const num of nums) {
+        sum += num;
+        const need = sum - k;
+        if (map.has(need)) {
+            count += (map.get(need) ?? 0)
+        }
+        map.set(sum, (map.get(sum) ?? 0) + 1)
+    }
+    return count;
+};
