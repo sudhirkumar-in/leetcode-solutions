3d500e72769de0d3a0c0a426f807d3db1d08c1be
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-06 06:35:32 +0530

    Time: 44 ms (8.75%), Space: 76.1 MB (5.39%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function (nums, k) {
+    let odds = nums.map(v => v % 2 == 1 ? 1 : 0);
+    let map = new Map();
+    map.set(0, 1);
+    let sum = 0;
+    let count = 0;
+    for (const num of odds) {
+        sum += num;
+        const need = sum - k;
+        if (map.has(need)) {
+            count += (map.get(need) ?? 0)
+        }
+        map.set(sum, (map.get(sum) ?? 0) + 1)
+    }
+    return count;
+};
