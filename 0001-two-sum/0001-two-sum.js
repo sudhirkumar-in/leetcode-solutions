b2d56203e2e957cfb66b0f6b523560309d2fb541
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-06 08:08:00 +0530

    Time: 0 ms (100%), Space: 59.2 MB (5.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,12 +5,11 @@
  */
 var twoSum = function (nums, target) {
     const map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+    for (const [i, num] of nums.entries()) {
+        const need = target - num
+        if (map.has(need)) {
+            return [map.get(need), i];
         }
-        map.set(nums[i], i);
+        map.set(num, i);
     }
-    return null
 };
