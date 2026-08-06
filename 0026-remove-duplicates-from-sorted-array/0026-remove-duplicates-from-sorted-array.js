de9a5432bb3de9745cc6e2f792f2dddf3854cd2e
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-06 11:29:52 +0530

    Time: 1 ms (48.97%), Space: 56.3 MB (95.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,18 +4,12 @@
  */
 var removeDuplicates = function (nums) {
     let left = 0;
-    let right = 1;
-    const res = []
     let n = nums.length;
-    while (right < n) {
+    for (let right = 1; right < n; right++) {
         if (nums[right - 1] != nums[right]) {
-            // replace right to left  
-            left++;
+            left++; // update left to right  
             nums[left] = nums[right];
         }
-        // every time right will go onword
-        right++;
     }
-
     return left + 1;
 };
