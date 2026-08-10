b24fef18b8cb5b8a077881d05e960880f315ad02
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-10 13:01:51 +0530

    Time: 2 ms (77.31%), Space: 58.9 MB (60.6%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -4,29 +4,23 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function (nums, k) {
+    if (k <= 1) return 0;
     let left = 0;
     let product = 1;
     let count = 0;
-
     for (let right = 0; right < nums.length; right++) {
-
         // incoming
         product *= nums[right]
-
         // shrink while invalid
         while (product >= k) {
-
             // outgoing
             product /= nums[left]
-
 
             left++;
         }
 
         // count valid subarrays ending at right
         count += right - left + 1
-
     }
-
-    return count < 0 ? 0 : count;
+    return  count;
 };
