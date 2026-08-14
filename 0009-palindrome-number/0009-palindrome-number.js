53a39e44156c40d80cb1d01cc733289d2f73e719
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-14 06:43:38 +0530

    Time: 4 ms (90.15%), Space: 63.4 MB (69.63%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let s = x.toString();
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
