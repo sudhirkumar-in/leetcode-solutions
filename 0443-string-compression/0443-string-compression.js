a8477e1016104e090e3bec969b1487d5089f4f9e
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-08-18 12:11:55 +0530

    Time: 5 ms (15.5%), Space: 58.8 MB (10.37%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -1,32 +1,23 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
 var compress = function (chars) {
-    chars.push('')
-    let n = chars.length
-
-    let ch = ''
-    let ct = 0
-    let res = []
-    for (let i = 0; i < n; i++) {
-        if (chars[i] !== chars[i - 1]) {
-            if (ct) {
-                res.push(ch)
-                if (ct > 1) {
-                    for (let e of ('' + ct)) {
-                        res.push(e)
-                    }
-                }
-            }
-
-            ch = chars[i]
-            ct = 1
+    let res = '';
+    let count = 1
+    for (let i = 0; i < chars.length; i++) {
+        if (chars[i] == chars[i + 1]) {
+            count++;
         } else {
-            ct++
+            res += chars[i] + (count === 1 ? '' : count);
+            // reset count 
+            count = 1;
         }
     }
-
-    let i = 0
-    for (let e of res) {
-        chars[i] = e
-        i++
+    chars.length = res.length;
+    for (let i = 0; i < res.length; i++) {
+        chars[i] = res[i]
     }
-    return res.length
+
+    return chars.length
 };
