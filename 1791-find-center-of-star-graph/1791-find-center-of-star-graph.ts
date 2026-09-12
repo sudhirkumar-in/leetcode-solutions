095a4ffe56e3ed47aa07fff009427774dba12f3f
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-12 11:17:54 +0530

    Time: 184 ms (8.33%), Space: 116.1 MB (5.55%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.ts
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.ts
@@ -7,13 +7,13 @@ function findCenter(edges: number[][]): number {
     const n = set.size;
     // console.log(n);
     const g = Array.from({ length: n + 1 }, () => []);
+    let maxLen = 0
     for (const [u, v] of edges) {
         g[u].push(v);
         g[v].push(u);
+        maxLen = Math.max(maxLen,g[u].length,g[v].length)
     }
-    let maxLen = Math.max(...g.map(v => v.length));
-    // console.log(maxLen)
-    // console.table(g);
+   
     for (let node = 0; node < n + 1; node++) {
         if (g[node].length === maxLen) {
             return node
