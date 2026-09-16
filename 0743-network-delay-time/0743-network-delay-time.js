e2acd80a721263a8d0887734f61644e928dcf5e2
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-16 15:38:31 +0530

    Time: 127 ms (31.05%), Space: 69.2 MB (27.91%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var networkDelayTime = function (times, n, k) {
+    // create a graph , adj list
+    const g = Array.from({ length: n + 1 }, () => []);
+    for (const [u, v, w] of times) {
+        g[u].push([v, w]);
+    }
+    // console.log(g);
+    const dist = new Array(n + 1).fill(Infinity);
+    dist[k] = 0;
+    const pq = new MinPriorityQueue((item) => item[1]);
+    pq.enqueue([k, 0]);
+    while (pq.size()) {
+        const [node, w] = pq.dequeue();
+        if (w > dist[node]) continue;
+        for (const [v, cw] of g[node]) {
+            if (w + cw < dist[v]) {
+                dist[v] = w + cw;
+                pq.enqueue([v, w + cw])
+            }
+        }
+    }
+    // console.log(dist)
+    // console.log(dist)
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        if (dist[i] === Infinity) return -1;
+        ans = Math.max(ans, dist[i]);
+    }
+    return ans;
+
+};
