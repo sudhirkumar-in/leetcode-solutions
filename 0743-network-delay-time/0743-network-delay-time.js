54b52a332263d351877d6a23dac9f3caa6b949d9
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-17 08:23:14 +0530

    Time: 111 ms (46.83%), Space: 68.4 MB (33.78%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.js
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -4,19 +4,26 @@
  * @param {number} k
  * @return {number}
  */
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
 var networkDelayTime = function (times, n, k) {
     // create a graph , adj list
     const g = Array.from({ length: n + 1 }, () => []);
     for (const [u, v, w] of times) {
         g[u].push([v, w]);
     }
-    console.log(g);
+    // console.log(g);
     const dist = new Array(n + 1).fill(Infinity);
     dist[k] = 0;
-    const pq = new MinPriorityQueue((a, b) => a[1] - b[1]);
+    const pq = new MinPriorityQueue((item) => item[1]);
     pq.enqueue([k, 0]);
     while (pq.size()) {
         const [node, w] = pq.dequeue();
+        if (w > dist[node]) continue;
         for (const [v, cw] of g[node]) {
             if (w + cw < dist[v]) {
                 dist[v] = w + cw;
@@ -24,6 +31,13 @@ var networkDelayTime = function (times, n, k) {
             }
         }
     }
-    console.log(dist)
-    return 1
+    // console.log(dist)
+    // console.log(dist)
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        if (dist[i] === Infinity) return -1;
+        ans = Math.max(ans, dist[i]);
+    }
+    return ans;
+
 };
