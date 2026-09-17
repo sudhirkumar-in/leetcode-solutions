a01ac1378a092ac467da1fb689fa3c8f8a191b6e
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-17 08:22:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.js
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -10,14 +10,13 @@ var networkDelayTime = function (times, n, k) {
     for (const [u, v, w] of times) {
         g[u].push([v, w]);
     }
-    // console.log(g);
+    console.log(g);
     const dist = new Array(n + 1).fill(Infinity);
     dist[k] = 0;
-    const pq = new MinPriorityQueue((item) => item[1]);
+    const pq = new MinPriorityQueue((a, b) => a[1] - b[1]);
     pq.enqueue([k, 0]);
     while (pq.size()) {
         const [node, w] = pq.dequeue();
-        if (w > dist[node]) continue;
         for (const [v, cw] of g[node]) {
             if (w + cw < dist[v]) {
                 dist[v] = w + cw;
@@ -25,13 +24,6 @@ var networkDelayTime = function (times, n, k) {
             }
         }
     }
-    // console.log(dist)
-    // console.log(dist)
-    let ans = 0;
-    for (let i = 1; i <= n; i++) {
-        if (dist[i] === Infinity) return -1;
-        ans = Math.max(ans, dist[i]);
-    }
-    return ans;
-
+    console.log(dist)
+    return 1
 };
