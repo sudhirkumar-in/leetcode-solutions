0b3294520d87f89d1f5cbce5dabcb14517d8832b
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-19 20:20:34 +0530

    Time: 1 ms (56.67%), Space: 62.2 MB (30.51%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,9 +11,24 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    function dfs(node) {
-        if (!node) return 0;
-        return Math.max(1 + dfs(node.left), 1 + dfs(node.right));
+    if (!root) return 0;
+    let levelCount = 0;
+    // bfs
+    let q = [root]
+    while (q.length) {
+        // increase the levelCount
+        levelCount++;
+        let level = q.length;
+        while (level--) {
+            const node = q.shift();
+            if (node.left) {
+                q.push(node.left);
+            }
+            if (node.right) {
+                q.push(node.right);
+            }
+        }
+
     }
-    return dfs(root);
+    return levelCount;
 };
