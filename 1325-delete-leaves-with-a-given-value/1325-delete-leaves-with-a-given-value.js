488491368f2563e03f7956f493d259b8aa4ef02b
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-22 08:42:07 +0530

    Time: 4 ms (10.14%), Space: 62.2 MB (20.29%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+var removeLeafNodes = function (root, target) {
+    function dfs(node) {
+        if (!node) return null;
+
+        node.left = dfs(node.left);    // whatever dfs decides left should be, assign it back
+        node.right = dfs(node.right);  // same for right
+
+        // NOW check: is this node a leaf, and does it match target?
+        // decide what to return: either `node` itself, or null
+        if (!node.left && !node.right && node.val === target) {
+            return null
+        }
+        return node
+
+    }
+    dfs(root);
+    if (!root.left && !root.right && root.val === target) return null
+    return root;
+};
