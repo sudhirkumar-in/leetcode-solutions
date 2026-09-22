ee2fb2603844404c94036f83e852eb476ede1a15
Sudhir Kumar <sudhirkumarlead@gmail.com>
2026-09-22 08:13:07 +0530

    Time: 1 ms (56.12%), Space: 61.7 MB (49.73%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,34 +11,9 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    if (!root) return 0;
-
-    const left = maxDepth(root.left);
-    const right = maxDepth(root.right);
-
-    // What should you return here?
-    return 1 + Math.max(left, right);
-};
-
-var maxDepth22 = function (root) {
-    if (!root) return 0;
-    let levelCount = 0;
-    // bfs
-    let q = [root]
-    while (q.length) {
-        // increase the levelCount
-        levelCount++;
-        let level = q.length;
-        while (level--) {
-            const node = q.shift();
-            if (node.left) {
-                q.push(node.left);
-            }
-            if (node.right) {
-                q.push(node.right);
-            }
-        }
-
+    function dfs(node) {
+        if (!node) return 0;
+        return 1 + Math.max(dfs(node.left), dfs(node.right)); // 1 for the node itself
     }
-    return levelCount;
+    return dfs(root);
 };
